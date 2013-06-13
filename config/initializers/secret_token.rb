0e955c5f6c54e877497c314bb155a8884309c803
h3idi <heidi.harman@gmail.com>
2013-06-13 23:16:36 +0200

    added bootstrapp-sass and liked routes for about and home

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treat::Application.config.secret_token = 'eaf11257d44a9612e5b409f7836d1adadad32a917760a19b51b63301c5ea5b4403d948bfc72a7eef4eaa73671ab293ade1bb4ac691ba2baeaec3d5d8d3bc660a'
