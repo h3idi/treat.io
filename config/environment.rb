1034ea7b0c71ab36a8aa57a98a0698e3b18a65c2
h3idi <heidi.harman@gmail.com>
2013-06-28 21:39:22 +0200

    remove image and heroku rake db:migrate

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Treat::Application.initialize!
+
+# gem rack-google-analytics
+config.gem 'rack-google-analytics', :lib => 'rack/google-analytics'
+config.middleware.use Rack::GoogleAnalytics, :tracker => 'UA-41988756-1'
+
+use Rack::GoogleAnalytics, :tracker => 'UA-41988756-1', :async => false
